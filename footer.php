dca4fd5e579a101d7e1f10f8925cabeae5b74c12
GAP00 <g.a.p@mail.ru>
2013-11-27 15:25:20 -0800

    Create footer.php
    clear version

---
+++ footer.php
@@ -0,0 +1,15 @@
+</div><!--wrapper-->
+<div class="clear"></div>
+<div id="footer">
+	<div id="footer-inside">
+		<p>
+			<?php _e('Copyright', 'paramitopia'); ?>&nbsp;&copy;&nbsp;<?php echo date("Y"); ?>&nbsp;<?php bloginfo('name'); ?>
+			&nbsp;|&nbsp;Theme <a href="http://paramitopia.com/blog/wordpress-themes/" title="designed by paramitopia">paramitopia</a>
+			&nbsp;|&nbsp;Powered by <a href="http://wordpress.org/">WordPress</a> 
+		</p>
+		<span id="back-to-top">&Delta; <a href="#nav" rel="nofollow" title="Back to top"><?php _e('Top', 'paramitopia'); ?></a></span>
+	</div>
+</div><!--footer-->
+<?php wp_footer(); ?>
+</body>
+</html>
